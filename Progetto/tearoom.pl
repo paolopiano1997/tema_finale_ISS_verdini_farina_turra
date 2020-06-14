f51f48c82b250b7429d1a7cae4fd4cc999763cde
paolopiano1997 <paolopiano1997@gmail.com>
2020-06-14 11:18:03 +0200

    First Sprint Problem Analysis

--- Progetto/tearoom.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% tearoom description   
-%====================================================================================
-context(ctxwaiter, "localhost",  "TCP", "8068").
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
- qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( waiter, ctxwaiter, "it.unibo.waiter.Waiter").
