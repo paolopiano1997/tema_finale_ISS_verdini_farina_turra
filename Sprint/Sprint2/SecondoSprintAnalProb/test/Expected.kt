22abb7757a8ff5e768d64f093577973f681337bc
Angelo <angi.farina@gmail.com>
2020-06-25 22:28:36 +0200

    testing

---
+++ Sprint/Sprint2/SecondoSprintAnalProb/test/Expected.kt
@@ -0,0 +1,9 @@
+object Expected{
+	val cleanStopped = "cleanStopped"
+	var tablePrevState = "dirty"
+	var cleanRemainingTime = "15"
+}
+/*
+public enum class Expected {
+    cleanStopped, dirty, undirty, sanitized
+}*/
---
+++ Sprint/Sprint2/SecondoSprintAnalProb/test/TestInterrupt.kt
@@ -0,0 +1,117 @@
+package test
+
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import org.junit.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+//import mapRoomKotlin.mapUtil
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.MsgUtil
+import it.unibo.kactor.MqttUtils
+import it.unibo.kactor.ApplMessage
+import it.unibo.kactor.ApplMessageType
+import it.unibo.waiterengine.Waiterengine
+import Expected
+
+
+
+
+class TestWaiterTableStates {
+	var waitermind            : ActorBasic? = null
+	var waitercleaner         : ActorBasic? = null
+	//val mqttTest   	      = MqttUtils("test") 
+	val initDelayTime     = 1000L
+
+	val enter = MsgUtil.buildRequest("waitermind","enter","enter","waitermind")
+	val clientready = MsgUtil.buildDispatch("waitermind","clientready","clientready","waitermind")	
+	val drinkready = MsgUtil.buildDispatch("waitermind","drinkready","drinkready","waitermind")
+	val paymentready = MsgUtil.buildDispatch("waitermind","paymentready","paymentready","waitermind")
+	val tableState = MsgUtil.buildDispatch("waitercleaner","tableState","tableState","waitercleaner")
+	val stoptimer = MsgUtil.buildDispatch("waitercleaner","stoptimer","stoptimer","waitercleaner")
+	
+	@kotlinx.coroutines.ObsoleteCoroutinesApi
+	@kotlinx.coroutines.ExperimentalCoroutinesApi
+	@Before
+	fun systemSetUp() {
+   		kotlin.concurrent.thread(start = true) {
+			it.unibo.ctxtearoom.main()
+		}
+	}
+
+	@After
+	fun terminate() {
+		println("%%%  TestWaiter terminate ")
+	}
+	
+	fun checkStop(){
+		if(waitermind!=null)
+			assert(waitermind!!.geResourceRep()==Expected.cleanStopped)
+	}
+	
+	fun checkCleaning(){ //controllo che NON sia in stop
+		if(waitermind!=null)
+			assert(waitermind!!.geResourceRep()!=Expected.cleanStopped)
+	}
+	
+	fun checkPrevState(){	//controllo che riprenda la clean dallo stato precedente
+		if(waitermind!=null)
+			assert(waitermind!!.geResourceRep()==Expected.tablePrevState)
+	}
+	
+	fun checkRemainingTime(){	//controllo che il timer segni il tempo effettivamente rimasto
+		if(waitermind!=null)
+			assert(waitermind!!.geResourceRep()==Expected.cleanRemainingTime) //TODO CAMBIARE EXPECTED
+	}
+	
+	@Test
+	fun testRobotboundary(){
+	 	runBlocking{
+ 			while( waitermind == null ){
+				println("testWaiterPosition waits for waitermind ... ")
+				delay(initDelayTime)  //time for robot to start
+				waitermind = it.unibo.kactor.sysUtil.getActor("waitermind")
+				waitercleaner = it.unibo.kactor.sysUtil.getActor("waitercleaner")
+ 			}
+			
+			MsgUtil.sendMsg(enter,waitermind!!)
+ 			delay(15000)
+			MsgUtil.sendMsg(clientready,waitermind!!)
+			delay(15000)
+			MsgUtil.sendMsg(drinkready,waitermind!!)
+			delay(15000)
+			MsgUtil.sendMsg(paymentready,waitermind!!)
+			delay(10000)
+			MsgUtil.sendMsg(enter,waitermind!!)	//INTERROMPO LA CLEAN
+			
+			
+			MsgUtil.sendMsg(tableState,waitercleaner!!)	//subito dopo l'interruzione ho bisogno di sapere lo stato a cui era rimasto il tavolo
+			//RICEZIONE E SALVATAGGIO in Expected.tablePrevState
+			MsgUtil.sendMsg(stoptimer,waitercleaner!!) //allo stesso modo salvo il tempo rimasto stoptimer
+			//RICEZIONE E SALVATAGGIO
+ 			delay(8000)
+			checkStop()	//controllo che il waiter abbia interrotto la clean
+			delay(10000)
+			MsgUtil.sendMsg(tableState,waitercleaner!!)	//mando un dispatch tablestate per ricevere lo stato del tavolo
+			//RICEZIONE risposta MsgUtil.???? contenente lo stato del tavolo
+			MsgUtil.sendMsg(stoptimer,waitercleaner!!) //chiedo il tempo rimasto
+			//RICEZIONE			
+			checkCleaning()	//controllo che riprenda la clean
+			checkPrevState()	//controllo che riprenda la clean dallo stato precedente
+			checkRemainingTime()//controllo che il timer segni il tempo effettivamente rimasto
+			
+			delay(20000) //da aggiustare in modo che corrisponda più o meno alla fine della clean
+			//controllo che una volta raggiunto sanitized NON interrompa la clean
+			MsgUtil.sendMsg(enter,waitermind!!)
+			checkCleaning() //controllo semplicemente se sta continuando a pulire
+			
+			
+			MsgUtil.sendMsg("end","end","end",waitermind!!)
+ 			if( waitermind != null ) waitermind!!.waitTermination()
+  		}
+	 	println("testWaiterPosition BYE  ")  
+	}
+}
